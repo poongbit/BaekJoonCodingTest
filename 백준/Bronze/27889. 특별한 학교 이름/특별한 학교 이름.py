ef6d5f4094442e9adc52a8bb86122327eb9e0335
Young Hwi Kim <42575458+poongbit@users.noreply.github.com>
2026-03-02 00:46:59 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.py
@@ -0,0 +1,22 @@
+"""
+NLCS: North London Collegiate School
+BHA: Branksome Hall Asia
+KIS: Korea International School
+SJA: St. Johnsbury Academy
+"""
+
+
+import sys
+
+
+
+key = input()
+
+
+univ_dic = {"NLCS":"North London Collegiate School", "BHA":"Branksome Hall Asia",
+            "KIS":"Korea International School","SJA":"St. Johnsbury Academy"}
+
+result = univ_dic[key]
+print(result)
+
+
