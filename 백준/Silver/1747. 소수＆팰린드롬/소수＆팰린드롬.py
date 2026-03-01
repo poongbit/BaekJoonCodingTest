a511ddd9d3c307bbd34c9250f9e82a570ca83307
Young Hwi Kim <42575458+poongbit@users.noreply.github.com>
2026-03-01 22:48:13 +0900

    [Silver I] Title: 소수&팰린드롬, Time: 2668 ms, Memory: 110536 KB -BaekjoonHub

--- 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.py
+++ 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.py
@@ -30,7 +30,7 @@ is_prime = [True] * limit
 is_prime[0] = False
 is_prime[1] = False
 
-for i in range(2,limit):
+for i in range(2,int(limit**0.5)+1):
     if is_prime[i]:
 
         for j in range(i*i,limit,i):
