1ef739da3f2db3e2326927eae94173e6bfbfe0f0
Young Hwi Kim <42575458+poongbit@users.noreply.github.com>
2026-03-04 02:09:19 +0900

    [Bronze V] Title: 대전과학고등학교를 사랑하십니까?, Time: 88 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/34691. 대전과학고등학교를 사랑하십니까？/대전과학고등학교를 사랑하십니까？.py
@@ -0,0 +1,17 @@
+import sys
+
+while True:
+    word = input()
+
+    if word == 'animal':
+        print('Panthera tigris')
+
+
+    elif word == 'flower':
+        print('Forsythia koreana')
+
+    elif word == 'tree':
+        print('Pinus densiflora')
+
+    else:
+        break
