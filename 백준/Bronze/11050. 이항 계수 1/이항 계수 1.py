119cda3c1ac6e66d07ab49b307af0d8908ac0b94
Young Hwi Kim <rladydgnj@gmail.com>
2026-03-08 22:18:22 +0900

    [Bronze I] Title: 이항 계수 1, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
@@ -1,31 +1,18 @@
 import sys
+input = sys.stdin.readline
 
+N,K = map(int,input().split())
 
-N,K= map(int,sys.stdin.readline().split())
+# 2차원 리스트를 0으로 초기화 (N+1 x N+1 크기)
+dp = [[0] * (N+1) for _ in range(N+1)]
 
-"""
-
-
-ex) (10)
-    (2)  = 10*9 / 2!
-
-
-"""
-
-
-
-result = 1
-
-for _ in range(K):
-    result= result * N
-    N = N-1
-
-result2 = 1
-
-for i in range(1,K+1):
-    result2 *= i 
-
-
-
-print(int(result/result2))
+for i in range(N+1):
+    dp[i][0] = 1  # i개 중에서 하나도 안 뽑을 경우
+    dp[i][i] = 1  # i개 중에서 i개 전부 다 뽑을 경우
     
+
+for i in range(2,N+1):
+    for j in range(1,i):
+        dp[i][j]  = dp[i-1][j-1] + dp[i-1][j]
+        
+print(dp[N][K])
