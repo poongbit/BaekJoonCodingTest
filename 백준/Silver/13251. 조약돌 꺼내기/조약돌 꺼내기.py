c813d2a37561150e131e419fb9e7ad04f8a736f6
Young Hwi Kim <rladydgnj@gmail.com>
2026-03-09 16:01:22 +0900

    [Silver III] Title: 조약돌 꺼내기, Time: 1624 ms, Memory: 692652 KB -BaekjoonHub

--- 백준/Silver/13251. 조약돌 꺼내기/조약돌 꺼내기.py
+++ 백준/Silver/13251. 조약돌 꺼내기/조약돌 꺼내기.py
@@ -30,7 +30,8 @@ for i in range(all_rocks+1):
 
 
 for i in range(2,all_rocks+1):
-    for j in range(1,i):
+    # j를 i까지 가지 말고 K까지만 가게 한다.
+    for j in range(1,min(i,K+1)):
         dp[i][j] = dp[i-1][j-1] + dp[i-1][j]
 
 # 전체 경우의 수 구하기 
