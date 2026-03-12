c9a010592c59b56189e9bd35061d8f4bb2cc6caa
Young Hwi Kim <rladydgnj@gmail.com>
2026-03-12 23:11:44 +0900

    [level 2] Title: 소수 찾기, Time: 983.84 ms, Memory: 51.5 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,33 +1,38 @@
 from itertools import permutations
-import math
 
-def is_prime(n):
-    
-    if n < 2:
-        return False
-    
-    for i in range(2,int(n**0.5) +1):
-        if n % i == 0:
-            return False
-        
-    return True
 
 
 def solution(numbers):
     answer = 0
     
-    # 길이 1짜리, 2짜리, 3자리.. 모든 순열을 만들어야 함
-    
     num_list = list(numbers)
+    num_length = len(num_list) # 숫자의 길이
     
-    found = set()
+    # 순열로 숫자 생성
     
-    for length in range(1,len(num_list)+1):
+    found = set() # 중복 숫자는 애초에 못 들어오지 하기 위함
+    
+    for length in range(1,num_length+1):
         for perm in permutations(num_list,length):
-            num = int(''.join(perm))
-            if is_prime(num) and num not in found:
-                answer +=1
-                found.add(num)
+            num = int(''.join(perm)) # 튜플로 되어 있는걸 조인시킴
+            found.add(num)
+            
+    # 에라토스테네스의 체
+    # 딱 한번만 생성한다. O(1)로 조회
+    max_num = max(found)
+    
+    is_prime = [True] * (max_num+1)
+    is_prime[0] = is_prime[1] = False
+    
+    for i in range(2,int(max_num**0.5)+1):
+        if is_prime[i]:
+            
+            for j in range(i*i,max_num+1,i):
+                is_prime[j] = False
                 
+    
+    for i in found:
+        if is_prime[i]:
+            answer +=1
     
     return answer
