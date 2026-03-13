21f47043961c492662ad272184a617da646fcf63
Young Hwi Kim <rladydgnj@gmail.com>
2026-03-14 02:04:19 +0900

    [level 3] Title: 네트워크, Time: 0.65 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,30 +1,23 @@
-def DFS(n,v,visited,computers):
-    
-    visited[v] = True
-    for i in range(n):
-        if computers[v][i] == 1 and not visited[i]:
-            DFS(n,i,visited,computers)
-            
-
-
-
 def solution(n, computers):
     
     answer = 0
-
-    # 1. 입력값 받기, 변수 생성
     
-   # DFS를 통해서 연결 상태를 확인한다.
-    
+    # 방문 도장 생성
     visited = [False] * n
     
-    # 모든 노드를 순회해가며 아직 방문 안한 노드 발견할 때마다 새 네트워크임
-    
-    for i in range(n): 
+    def DFS(v):
+        # 방문 도장 쾅 찍기
+        visited[v] = True
+        
+        for i in range(n): # 모든 노드를 훑으면서
+            if computers[v][i] == 1 and not visited[i]:
+                visited[i] = True
+                DFS(i)
+        
+    for i in range(n): # 0-based index
         if not visited[i]:
-            DFS(n,i,visited,computers)
-            answer +=1 # 네트워크 하나 발견
-    
+            DFS(i)
+            answer +=1
     
     
     return answer
