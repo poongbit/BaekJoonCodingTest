07514a2aaa9ef9833fe5a72e56fd862874d9d50f
Young Hwi Kim <rladydgnj@gmail.com>
2026-03-14 18:39:17 +0900

    [level 3] Title: 네트워크, Time: 0.52 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,25 +1,27 @@
 def solution(n, computers):
     
     answer = 0
-    
-    # 방문 도장 생성
+
     visited = [False] * n
+
     
     def DFS(v):
-        # 방문 도장 쾅 찍기
         visited[v] = True
-        
-        for i in range(n): # 모든 노드를 훑으면서
-            if computers[v][i] == 1 and not visited[i]:
-                visited[i] = True
-                DFS(i)
-        
-    for i in range(n): # 0-based index
+                    
+        for i in range(n):
+            if not visited[i]:
+                if computers[v][i] == 1:
+                    DFS(i)
+                    
+    
+    for i in range(n):
         if not visited[i]:
             DFS(i)
             answer +=1
     
     
+        
+
     return answer
     
     
