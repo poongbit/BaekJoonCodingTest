dd4311ddcc4691400114343d0f2e0b71563dd9d8
Young Hwi Kim <rladydgnj@gmail.com>
2026-03-14 13:55:44 +0900

    [level 2] Title: 타겟 넘버, Time: 367.47 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,25 +1,30 @@
-count = 0
-
-def DFS(index,current_sum,numbers,target):
-    
-    if index == len(numbers):
-        if current_sum == target:
-            global count
-            count +=1
-
-        return
-
-    DFS(index+1,current_sum + numbers[index],numbers,target)
-    DFS(index+1,current_sum - numbers[index],numbers,target)
-    
 
 
 def solution(numbers, target):
     answer = 0
     
-    DFS(0,0,numbers,target)
+    # 1. 입출력 받기
     
-    answer = count
+    count = 0
     
+    def DFS(index,current_sum):
+        # 모든 숫자를 써야 다 카운트
+        if index == len(numbers):
+            if current_sum == target:
+                # count는 지역변수 복사본이라 밖으로 안나감
+                return 1
+                
+            else:
+                return 0
+            
+        plus = DFS(index+1, current_sum + numbers[index])
+        minus = DFS(index+1, current_sum - numbers[index])
+        
+        return plus + minus
+    
+    
+            
+    
+    answer = DFS(0,answer)
 
     return answer
