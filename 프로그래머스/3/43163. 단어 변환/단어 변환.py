cac0c58279960f734c5c11e26b56d5610db7f1c7
Young Hwi Kim <rladydgnj@gmail.com>
2026-03-15 16:08:27 +0900

    [level 3] Title: 단어 변환, Time: 0.49 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -3,41 +3,60 @@ from collections import deque
 def solution(begin, target, words):
     answer = 0
     
+    # 무엇을 결정해야 하는 가?
+    # 알파벳 하나만 바꿔서 words에 있는 걸로 바꿀 수 있는 지 
     
-    if target not in words:
-        return 0
+    # 종료 조건은 무엇인가?
+    # 바꾼 단어가 target과 똑같은지
     
-    # 단어 수 만큼 방문 도장 찍기
+    # 끝났을 떄 뭘 체크해야 하는가?
+    
+    # 단어들을 다 체크했는 지 확인
     visited = [False] * len(words)
     
-    
-    # 두 단어가 한 글자만 다른지 체크하는 함수
+    # 알파벳 차이가 1일 때 단어를 변경함
     def is_convertible(a,b):
         diff = 0
         for i in range(len(a)):
             if a[i] != b[i]:
                 diff +=1
+                
+        return True if diff == 1 else False
     
-        return diff == 1
     
-    q = deque()
-    q.append((begin,0))
+    # words의 방문 도장
+    visited = [False] * (len(words))
     
-    while q:
-        word, count = q.popleft()
+    def BFS():
+        # 큐 스택 초기화
+        q = deque()
         
-        if word == target:
-            return count
+        
+        count = 0 
+        
+        q.append((begin,count))
+        
+        while q:
+            start, count = q.popleft()
+            
+            if start == target:
+                return count
+            
+            
+            for i in range(len(words)):
+                if not visited[i] and is_convertible(start, words[i]):
+                    visited[i] = True
+                    q.append((words[i],count+1))
         
         
-        for i in range(len(words)):
-            if not visited[i] and is_convertible(word,words[i]):
-                visited[i] = True
-                q.append((words[i],count+1))
+        return 0
+        
+        
+    answer = BFS()
+        
+        
     
-    
-    return 0
-    
+    return answer
 
     
     
