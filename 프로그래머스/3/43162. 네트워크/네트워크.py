1cc70daa59dae9c8367afd8857e1b59c730d7b7b
Young Hwi Kim <rladydgnj@gmail.com>
2026-03-15 14:51:01 +0900

    [level 3] Title: 네트워크, Time: 0.37 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -4,11 +4,14 @@ def solution(n, computers):
     
     visited = [False] * n
     
-    def DFS(v):
-        visited[v] = True
+    def DFS(node):
+        # 노드 방문 완료
+        visited[node] = True 
         
         for i in range(n):
-            if not visited[i] and computers[v][i] == 1:
+            # i 노드에 방문을 하지 않았고, node와 i가 연결되어 있다면
+            if not visited[i] and computers[node][i] == 1: 
+                visited[i] = True
                 DFS(i)
     
     for i in range(n):
