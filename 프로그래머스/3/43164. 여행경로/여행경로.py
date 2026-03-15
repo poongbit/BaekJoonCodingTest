ffb745c8d0303e7e1e201db8eef1e5df4342b14c
Young Hwi Kim <rladydgnj@gmail.com>
2026-03-15 17:10:13 +0900

    [level 3] Title: 여행경로, Time: 288.30 ms, Memory: 13.7 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.py
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -2,35 +2,34 @@ from collections import defaultdict
 
 def solution(tickets):
     answer = []
-
-    airport_graph = defaultdict(list)
     
-    # 티켓 번호에 대응된다.
+    # 티켓 사용 여부
     visited = [False] * len(tickets)
     
-    # 1. 입력값 입력 받기
-    for a,b in tickets:
-        airport_graph[a].append(b)
     
+    # 주어진 티켓을 다 활용해야 하고, 다 방문했는 지 체크해야 함
+    # 티켓을 다 사용하면 모든 도시를 방문할 수 있음 
     
-    def DFS(current,route):
-        # 1. 종료 조건
-        # 티켓은 3장, 경로는 4장
+    def DFS(start,route):
         if len(route) == len(tickets) + 1:
             answer.append(route)
             return
         
-        # 2. 티켓 순회
-        
         for i in range(len(tickets)):
-            if not visited[i] and tickets[i][0] == current:
-                
-                # 3. 방문 처리 + 재귀 + 백트래킹
+            # 티켓을 사용하지 않았고, 출발지가 그 티켓의 출발지와 같은 경우
+            if not visited[i] and (start == tickets[i][0]):
                 visited[i] = True
                 DFS(tickets[i][1], route + [tickets[i][1]])
+                
+                # 다시 돌아올 때 백 트래킹 필요
                 visited[i] = False
-        
-    DFS("ICN",["ICN"]) # 항상 ICN 출발, 경로에 ICN 포함
+                
+    
+    DFS("ICN",["ICN"])
     
     
-    return sorted(answer)[0] # 알파벳 순서 앞서는 기준으로
+    return sorted(answer)[0]
+    
+    
+    
+    
