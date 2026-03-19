43722b5baeffa6d4abd878cae9d69a7986f54247
Young Hwi Kim <rladydgnj@gmail.com>
2026-03-19 23:48:39 +0900

    [level 3] Title: 베스트앨범, Time: 0.06 ms, Memory: 9.19 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -4,39 +4,33 @@ from collections import defaultdict
 def solution(genres, plays):
     answer = []
     
-    # 장르별 총 재생 수
-    total = defaultdict(int)
+    # 가장 많이 팔린 순서대로 장르 순 구하기
+    music_genre = defaultdict(int)
     
     for i in range(len(plays)):
-        total[genres[i]] += plays[i]
+        music_genre[genres[i]] += plays[i]
+    
+    # 가장 많이 팔린 순으로 정렬
+    music_genre = sorted(music_genre.items(), key = lambda x : x[1], reverse = True)
     
     
-    total = sorted(total.items(),key= lambda x : x[1],reverse = True)
-    
-    
-    # 장르별 곡 목록 (고유번호, 재생 수)
-    music_number = defaultdict(list)
+    # 한 음악 장르에서 가장 많이 들은 음악 순으로 정렬
+    genre_played = defaultdict(list)
     
     for i in range(len(plays)):
-        music_number[genres[i]].append((i,plays[i]))
+        genre_played[genres[i]].append((i,plays[i]))
         
     
+    # 각 key값마다 value에서 정렬하기
     
-    # 장르 총 재생 수 내림차순 정렬, 장르별로 따로 정렬해야 함
-    
-    for genre in music_number.keys():
-        music_number[genre] = sorted(music_number[genre],key = lambda x : x[1],reverse= True)
+    for key in genre_played.keys():
+        genre_played[key] = sorted(genre_played[key], key = lambda x : x[1],reverse = True)
         
     
-    
-    for genre, _ in total:
-        for idx, _ in music_number[genre][:2]:
-            # pop 일 때 : [(4,2500),(1,600)]
-            # classic 일 때 : [(3,800),(0,500)]
+    for key, _ in music_genre:
+        for index, played in genre_played[key][:2]:
+            answer.append(index)
             
-            answer.append(idx)
-            
-    
     
 
     return answer
