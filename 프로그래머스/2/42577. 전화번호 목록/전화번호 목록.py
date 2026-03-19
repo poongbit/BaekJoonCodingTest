32b85ad60bf2c32822a0f706006f77c56df77a04
Young Hwi Kim <rladydgnj@gmail.com>
2026-03-19 18:32:14 +0900

    [level 2] Title: 전화번호 목록, Time: 504.07 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,19 +1,16 @@
-from collections import defaultdict
-
 def solution(phone_book):
     answer = True
     
-    
+    # 반복적으로 번호를 탐색해야 하므로
     set_phonebook = set(phone_book)
     
-    
     for word in set_phonebook:
-        for j in range(1,len(word)):
-            
-            if word[:j] in set_phonebook:
+        
+        word_length = len(word)
+        
+        for i in range(1,len(word)):
+            if word[:i] in set_phonebook:
                 answer = False
                 break
-            
-    
     
     return answer
