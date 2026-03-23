00082c62abc82e3039b53e9b56589915dd5a7443
Young Hwi Kim <rladydgnj@gmail.com>
2026-03-23 20:42:11 +0900

    [level 3] Title: 단어 변환, Time: 0.45 ms, Memory: 9.36 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,62 +1,53 @@
 from collections import deque
-
 def solution(begin, target, words):
-    answer = 0
     
-    # 무엇을 결정해야 하는 가?
-    # 알파벳 하나만 바꿔서 words에 있는 걸로 바꿀 수 있는 지 
+    """
+    3개중 두 개 맞으면 True로 반환하는 함수 만들기
     
-    # 종료 조건은 무엇인가?
-    # 바꾼 단어가 target과 똑같은지
+    # 멈춤 조건:
+        바꾼게 타겟과 같으면 멈춤
+        맞으면 반환
     
-    # 끝났을 떄 뭘 체크해야 하는가?
+    """
     
-    # 단어들을 다 체크했는 지 확인
     visited = [False] * len(words)
     
-    # 알파벳 차이가 1일 때 단어를 변경함
+    
     def is_convertible(a,b):
-        diff = 0
+        num = 0
         for i in range(len(a)):
             if a[i] != b[i]:
-                diff +=1
-                
-        return True if diff == 1 else False
-    
-    
-    # words의 방문 도장
-    visited = [False] * (len(words))
-    
-    def BFS():
-        # 큐 스택 초기화
+                num +=1
+        
+        if num ==1:
+            return True
+        else:
+            return False
+        
+    def DFS():
         q = deque()
-        
-        
-        count = 0 
-        
-        q.append((begin,count))
-        
+        q.append(begin)
+        count = 0
+
         while q:
-            start, count = q.popleft()
-            
-            if start == target:
+            now_word = q.popleft()
+
+            if now_word == target:
                 return count
-            
-            
+
             for i in range(len(words)):
-                if not visited[i] and is_convertible(start, words[i]):
+                if is_convertible(now_word,words[i]) and not visited[i]:
                     visited[i] = True
-                    q.append((words[i],count+1))
-        
+                    count +=1
+                    q.append(words[i])
         
         return 0
-        
-        
-    answer = BFS()
-        
-        
     
+    
+    answer = DFS()
+        
     return answer
+        
 
     
     
