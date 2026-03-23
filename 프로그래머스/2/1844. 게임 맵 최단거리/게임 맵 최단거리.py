4054525df5b760524dd39a33c81ab3b78de9b964
Young Hwi Kim <rladydgnj@gmail.com>
2026-03-23 16:47:22 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 12.13 ms, Memory: 9.43 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,48 +1,43 @@
 from collections import deque
 
-
 def solution(maps):
-    
     answer = 0
     
     # 방문 도장 생성하기
+    # 맵 바깥으로 나가면 안되므로 가로 세로 길이 저장
     n,m = len(maps), len(maps[0])
     
     visited = [[0] * m for _ in range(n)]
     
-    # 1,1은 방문했으므로 표기함
-    visited[0][0] = 1
+    # 칸의 개수의 최솟값 = 최단거리
+    # BFS를 이용하기
     
-    # 큐 스택 초기화
+    
+    # 1을 0으로 바꿔서 방문 체크
     q = deque()
     
-    # 좌표(x,y), 거리 초기화
     q.append((0,0,1))
     
-    # 상,하,좌,우
-    dx = [0,0,-1,1]
-    dy = [-1,1,0,0]
-    
-    # maps[행][열]
-    
     while q:
-        now_x, now_y , move = q.popleft()
+        now_x,now_y,distance = q.popleft()
         
-        if now_x == m-1 and now_y == n-1:
-            return move
+        # 상,하,좌,우
+        dx = [0,0,-1,1]
+        dy = [-1,1,0,0]
         
         for i in range(4):
-            x = now_x + dx[i]
-            y = now_y + dy[i]
+            new_x = now_x + dx[i]
+            new_y = now_y + dy[i]
             
-            if 0<=x<m and 0<=y<n and maps[y][x] == 1 and not visited[y][x]:
-                visited[y][x] = True
-                q.append((x,y,move+1))
-    
-    
+            if (0<=new_x<m and 0 <=new_y < n):
+                if maps[new_y][new_x] == 1:
+                    if new_x == m-1 and new_y == n-1:
+                        return distance + 1
+                    
+                    # 이제 지나간 길
+                    maps[new_y][new_x] = 0
+                    q.append((new_x,new_y,distance +1))
+            
     return -1
+                
 
-
-    
-
-    
