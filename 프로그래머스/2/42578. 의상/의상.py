21ffb991561f1193b2e8a5f10854cdb0fa7c03e2
Young Hwi Kim <rladydgnj@gmail.com>
2026-03-24 17:11:11 +0900

    [level 2] Title: 의상, Time: 0.03 ms, Memory: 9.12 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,44 +1,21 @@
-from itertools import combinations
 from collections import defaultdict
 
 def solution(clothes):
     answer = 0
     
-    wear_style = defaultdict(int)
-    kind_style = set()
+    # 사전으로 종류와 개수를 기록하고
+    # 경우의 수로 곱한다. 0,1,2개 선택하는 경우까지
     
+    fashion = defaultdict(int)
     
-    # 값 부여하기
-    for item, style in clothes:
-        wear_style[style] += 1
+    for cloth, style in clothes:
+        fashion[style] +=1
         
-    # 곱셈 누적
+    # 곱하기를 위한 변수 선언
     result = 1
-    for count in wear_style.values():
-        result *= (count + 1) # 안 입음 포함
-        
-    answer = result -1
-        
+    for style, count in fashion.items():
+        result *= count + 1 #(아무 것도 선택 안하는 것 포함)
     
-    
-    
-    
-    """
-    최소 한 가지 옷을 입는다.
-    
-    ex) headgear 종류 2가지, eyewear 1가지
-    headgear a, b, 아무것도 안 입기 - 3
-    eyewear c, 아무것도 안 입기 - 2
-    
-    3 x 2 = 6, 여기서 둘 다 안 입기는 빼야 하므로 -1 해야 함
-    
-    
-    """
-
-    
-    
-    
-    
-    
+    answer = result - 1 #(전부 다 안 고른 경우 뺴줌)
     
     return answer
