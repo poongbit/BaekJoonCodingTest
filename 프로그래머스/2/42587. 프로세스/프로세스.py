b74bd2d3cd02a6917e067afb9b3c17ebbac5a9b4
Young Hwi Kim <rladydgnj@gmail.com>
2026-03-25 00:18:27 +0900

    [level 2] Title: 프로세스, Time: 2.99 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -6,26 +6,31 @@ def solution(priorities, location):
     # 큐 생성
     q = deque()
     
-    # 우선 순위, 인덱스(location)을 큐에 넣음
     for i in range(len(priorities)):
+        # 우선순위, 인덱스 추가 
         q.append((priorities[i],i))
-        
+    
+    # [2, 1, 3, 2], location = 2
+    
+    count = 0
+    
+    #print(q[0])
+    # 	(2, 0)
     
     while q:
-        now, index = q.popleft()
+        base, index = q.popleft()
         
-        # 큐가 있고, now가 max에서의 우선 순위보다 작다면
-        if q and now < max(q,key = lambda x: x[0])[0]:
-            # 방금 꺼낸 걸 다시 넣는다.
-            q.append((now,index))
-    
-        else:
-            # 방금 꺼낸걸 실행했으므로, 번호표 발급
-            answer +=1
+        # 전 순회를 해서 base가 가장 높은 우선순위 인지를 체크
+        
+        if q and base < max(q,key=lambda x: x[0])[0]:
+            # 다시 집어넣기
+            q.append((base,index))
             
-            # 그 꺼낸 큐가 우리가 원한 그 location이면 answer 반환
+        else:
+            count +=1
             if index == location:
-                return answer
+                answer = count
+                break
     
     
     return answer
