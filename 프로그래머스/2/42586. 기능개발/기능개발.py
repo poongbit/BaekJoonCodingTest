fc1ab3d108fc793c34a980023a4c6e059ba23221
Young Hwi Kim <rladydgnj@gmail.com>
2026-03-24 22:35:06 +0900

    [level 2] Title: 기능개발, Time: 0.09 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -4,29 +4,36 @@ import math
 def solution(progresses, speeds):
     answer = []
     
-    left_work = []
+    # 일이 나가는 순서 - FIFO
+    # 배포 타이밍은 앞에 있는 일이 진도율이 꽉 찰 떄 다 같이 나가는 걸로
     
-    for i in range(len(progresses)):
-        days = math.ceil((100-progresses[i])/ speeds[i])
-        left_work.append(days)
+    # 1. 각 progress 마다의 진도율 계산, 배포 되는 타이밍 체크
+    left_days = []
+    
+    for i in range(len(speeds)):
+        progress_left = math.ceil((100-progresses[i]) / speeds[i])
+        left_days.append(progress_left)
+    
+    q = deque()
+    
+    # left days에 큐 집어 넣기
+    
+    for i in range(len(left_days)):
+        q.append(left_days[i])
         
-    # ex) [7,3,9]
-    q = deque(left_work)
-    
     while q:
-        
         base = q.popleft()
-        
-        # popleft 하면서 한 명 나옴
         count = 1
         
         while q and q[0] <= base:
-            # base보다 일이 덜 걸려서 이미 일이 끝난 사람들도 같이 걸려서 나옴
-            q.popleft()
+            q.popleft() # base보다 걸리는 작업이 적으면 같이 나감
             count +=1
-        
-        # 묶인 사람들끼리 나옴
+            
         answer.append(count)
         
-
+        
+    
+    
+    
+    
     return answer
