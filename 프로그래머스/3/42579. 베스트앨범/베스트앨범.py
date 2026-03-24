03bf336c96b43c4c22f848844e6c23b046a0c652
Young Hwi Kim <rladydgnj@gmail.com>
2026-03-24 20:51:16 +0900

    [level 3] Title: 베스트앨범, Time: 0.06 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,36 +1,58 @@
 from collections import defaultdict
 
-
 def solution(genres, plays):
     answer = []
     
-    # 가장 많이 팔린 순서대로 장르 순 구하기
-    music_genre = defaultdict(int)
+    """
+    클래식, 팝 둘 중 어느게 가장 많이 들었는 지 확인
+    딕셔너리 활용?
+    
+    그 후, 각 카테고리 별로 plays 고유 번호 순으로 정렬하기 
+    딕셔너리 내에서 정렬시키기?
+    
+    """
+    
+    music_played = defaultdict(int)
     
     for i in range(len(plays)):
-        music_genre[genres[i]] += plays[i]
+        music_played[genres[i]] += plays[i]
     
-    # 가장 많이 팔린 순으로 정렬
-    music_genre = sorted(music_genre.items(), key = lambda x : x[1], reverse = True)
+    # 가장 많은 카테고리 순으로 정렬하기
+    result = []
+        
+    for key, value in music_played.items():
+        result.append((key,value))
+    
+    result.sort(key=lambda x : x[1], reverse = True)
     
     
-    # 한 음악 장르에서 가장 많이 들은 음악 순으로 정렬
-    genre_played = defaultdict(list)
+    music_genre = []
     
-    for i in range(len(plays)):
-        genre_played[genres[i]].append((i,plays[i]))
+    for key, value in result:
+        music_genre.append(key)
+    
+     # genres, plays와 1 대 1 매칭시키기?
+    
+    music_list = defaultdict(list)
+    
+    
+    for i in range(len(genres)):
+        # 재생 수, 고유 번호 연결
+        music_list[genres[i]].append((plays[i],i))
         
     
-    # 각 key값마다 value에서 정렬하기
-    
-    for key in genre_played.keys():
-        genre_played[key] = sorted(genre_played[key], key = lambda x : x[1],reverse = True)
+    for genre in music_genre:
+        count = 1
+        music_list[genre].sort(key = lambda x : x[0],reverse = True)
         
-    
-    for key, _ in music_genre:
-        for index, played in genre_played[key][:2]:
+        for plays, index in music_list[genre]:
+            if count > 2:
+                break
             answer.append(index)
-            
+            count +=1
+    
+    
+    
     
 
     return answer
