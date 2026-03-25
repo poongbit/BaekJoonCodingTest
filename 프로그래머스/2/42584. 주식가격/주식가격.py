bfaeae4cbcc26fe8ba51e8cf3ee3335e726aeea6
Young Hwi Kim <rladydgnj@gmail.com>
2026-03-25 17:06:59 +0900

    [level 2] Title: 주식가격, Time: 24.98 ms, Memory: 19 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,17 +1,22 @@
 def solution(prices):
-    answer = []
+    answer = [0] * len(prices)
     
-    for i in range(len(prices)):
-        count = 0
-        for j in range(i+1,len(prices)):
-            if prices[i] > prices[j]:
-                count +=1
-                break
-            else:
-                count +=1
-                
-        answer.append(count)
+    # 인덱스 저장하는 변수
+    stack = []
     
+    for i, price in enumerate(prices):
+        # 현재 가격이 마지막에 저장했던 price[index] 보다 작으면, 떨어진 것
+        while stack and prices[stack[-1]] > price:
+            j = stack.pop()
+            answer[j] = i - j
+            
+        stack.append(i)
+    
+    # 어디 걸리는 거 없이 끝까지 증가한 price들의 인덱스
+    while stack:
+        j = stack.pop()
+        
+        answer[j] = len(prices)-1 - j
     
     return answer
             
