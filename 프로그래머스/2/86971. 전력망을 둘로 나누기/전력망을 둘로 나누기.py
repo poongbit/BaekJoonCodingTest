663cec7098a3986760b431e2685c27006335f63f
Young Hwi Kim <rladydgnj@gmail.com>
2026-03-27 14:21:38 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 7.05 ms, Memory: 9.19 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,59 +1,45 @@
-from collections import deque
-
-
-def BFS(n,v,graph):
-    
-    # 큐 스택 초기화
-    q = deque()
-    
-    # 출석부 작성
-    visited = [False] * (n+1)
-    
-    visited[v] = True
-    
-    # 초기 스택에 들어감
-    q.append(v)
-    
-    size = 1
-    
-    while q:
-        now_node = q.popleft()
-        
-        for next_node in graph[now_node]:
-            if visited[next_node] != True:
-                visited[next_node] = True
-                size +=1
-                q.append(next_node)
-                
-    return size
-        
-
-
-
+import math
 def solution(n, wires):
     answer = n
     
-    
-    for i in range(len(wires)):
-        # i 번쨰 간선만 제외하고 그래프 구성
-        tree_graph = [[] * (n+1) for _ in range(n+1)]
+    def DFS(v,graph):
+        # 송전탑 방문 성공
+        visited[v] = True
+        count = 1
         
-        for j in range(len(wires)):
+        for next_node in graph[v]:
+            if not visited[next_node]:
+                count += DFS(next_node,graph) # 누적
+                
+
+        return count
+    
+    
+    # i번쨰 wire를 끊어버린다는 가정 하에
+    for i in range(len(wires)):
+        w_graph = [[] for _ in range(n+1)]
+        
+        for j,(v1,v2) in enumerate(wires):
+            # i번째 전력망이면 그냥 데이터 안 넣고 넘어감
             if i == j:
                 continue
-                
-            else:
-                a,b = wires[j][0], wires[j][1]
-                
-                tree_graph[a].append(b)
-                tree_graph[b].append(a)
-    
-       
-        new_size = BFS(n,1,tree_graph)
-        diff = abs(new_size - (n - new_size))
-                
-        if diff < answer:
-            answer = diff
+            w_graph[v1].append(v2)
+            w_graph[v2].append(v1)
+            
+        # 송전탑 방문 기록 선언하기
+        visited = [False] * (n+1)
+        
+        # 횟수 세기
+        count = 0
+        
+        result = DFS(1,w_graph)
+        
+        other_side = n - result
+        
+        diff = abs(result - other_side)
+        
+        answer = min(diff,answer)
+            
         
     
     return answer
