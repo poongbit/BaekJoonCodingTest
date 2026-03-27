ac7af1945dbca8497e3ee54a388f05bca1fb8551
Young Hwi Kim <rladydgnj@gmail.com>
2026-03-27 15:25:46 +0900

    [level 2] Title: 바이러스 파이프, Time: 1.50 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/2/468373. 바이러스 파이프/바이러스 파이프.py
+++ 프로그래머스/2/468373. 바이러스 파이프/바이러스 파이프.py
@@ -1,55 +1,53 @@
 from collections import deque
-
+import math
 def solution(n, infection, edges, k):
-    answer = 0
-
-    # 1. 양방향 그래프 생성
-    virus_graph = [[] for _ in range(n+1)]
+    answer = [0] * n
+    
+    # 1. 양방향 그래프 구성
+    infected_graph = [[] for _ in range(n+1)]
     
     for x,y,p_type in edges:
-        virus_graph[x].append((y,p_type))
-        virus_graph[y].append((x,p_type))
+        infected_graph[x].append((y,p_type))
+        infected_graph[y].append((x,p_type))
     
-    # 2. spread 함수- BFS로 감염 전파
     
-    def spread(infected_set,p_type):
-        # 새로운 new_infected
+    # 2. spread 함수 - BFS로 타입 열면 감염 전파
+    
+    def spread(infected_group,pipe_type):
+        q = deque(infected_group)
         
-        # 새로운 거는 기존에 것과 추가할 수 있도록 복사해둠
-        new_infected = set(infected_set)
-        
-        q = deque(infected_set)
+        new_infected = set(infected_group)
         
         while q:
             now_node = q.popleft()
-            for next_node, next_type in virus_graph[now_node]:
-                # 다음 타입이 spread할 타입과 같고, new_infected에 없는 새 노드
-                if next_type == p_type and next_node not in new_infected:
+            
+            for next_node,p_type in infected_graph[now_node]:
+                if next_node not in new_infected and p_type == pipe_type:
                     new_infected.add(next_node)
                     q.append(next_node)
-        
+                    
         return new_infected
+        
     
-    answer = [0]
     
-    # 3. dfs 함수 호출 - > 타입 순열 검색
-    def DFS(remaining, infected_set, prev_type):
-        answer[0] = max(answer[0],len(infected_set))
-        # 반복이 다 끝났으면 종료
+    
+    # 3. DFS 함수 - 타입 선택 순서 완전탐색
+    def DFS(remaining,infected,pipe_type):
+        answer[0] = max(answer[0],len(infected))
         if remaining == 0:
-            return 
+            return
         
-        # 타입 1,2,3
-        for p_type in [1,2,3]:
-            if p_type == prev_type:
+        for item in [1,2,3]:
+            if item == pipe_type:
                 continue
-            new_infected = spread(infected_set,p_type)
-            DFS(remaining-1,new_infected,p_type)    
+                
+            new_infected = spread(infected,item)
+            
+            DFS(remaining-1,new_infected,item)    
     
     
-    # 4. dfs 호출 - 리턴
-        
-    DFS(k,{infection},None)
+    # 4. DFS 실행 + return
+    DFS(k,{infection},None)    
     
     
     return answer[0]
