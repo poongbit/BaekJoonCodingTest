2fbb3514411bd8b238baff938d9185bab2ad4784
Young Hwi Kim <rladydgnj@gmail.com>
2026-03-29 01:29:29 +0900

    [Silver II] Title: 사탕 게임, Time: 1440 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/3085. 사탕 게임/사탕 게임.py
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.py
@@ -3,47 +3,52 @@ input = sys.stdin.readline
 
 n = int(input().strip())
 
-candy_graph = [[0] * (n) for _ in range(n)]
+graph = [[0] * (n) for _ in range(n)]
 
 for i in range(n):
     line = input().strip()
 
     for j in range(len(line)):
-        candy_graph[i][j] = line[j]
+        graph[i][j] = line[j]
 
 
 
-def max_count(graph):
-    # 모든 행, 모든 열을 순회하면서
-    # 연속으로 같은 색이 몇 개인지 세기
-    # 최댓값 변환
+# 복잡한 것이 있다면 함수로 지정하라
+
+
+
+def change(graph):
 
     count = 0
 
-    # 행 방향 비교하기
-    for i in range(len(graph)):
+    # 행 기준으로 바꾸기
+    for i in range(n):
         cur = 1
-        for j in range(1,len(graph)):
-            # 옆에꺼 비교하기
+        
+        for j in range(1,n):
             if graph[i][j] == graph[i][j-1]:
                 cur +=1
 
             else:
-                cur = 1 # 연속 끊기면 리셋
+                # 이어진게 끊어짐
+                cur = 1
 
-            count = max(count,cur)
 
+            count = max(cur,count)
 
-    for i in range(len(graph)):
+    # 열 기준으로 바꾸기
+    for i in range(n):
         cur = 1
-        for j in range(1,len(graph)):
+
+        for j in range(1,n):
             if graph[j][i] == graph[j-1][i]:
                 cur +=1
 
             else:
-                cur = 1 # 연속 끊기면 리셋
+                # 이어진게 끊어짐
+                cur = 1
 
-            count = max(count,cur)
+            count = max(cur,count)
 
 
     return count
@@ -53,15 +58,22 @@ ans = 0
 for i in range(n):
     for j in range(n):
 
-        if j + 1 < n and candy_graph[i][j] != candy_graph[i][j+1]:
-            candy_graph[i][j], candy_graph[i][j+1] = candy_graph[i][j+1], candy_graph[i][j]
-            ans = max(ans,max_count(candy_graph))
-            candy_graph[i][j], candy_graph[i][j+1] = candy_graph[i][j+1], candy_graph[i][j]
+        # 행 기준으로 한번 체크
+        # 인덱스를 안 벗어나고, 그 두개가 다를 경우
+        if j+1<n and graph[i][j] != graph[i][j+1]:
+            # 스왑해보기
+            graph[i][j], graph[i][j+1] = graph[i][j+1], graph[i][j]
+            ans = max(ans, change(graph))
+            # 다시 원래대로 복구하기
+            graph[i][j], graph[i][j+1] = graph[i][j+1], graph[i][j]
 
-        if i+1 < n and candy_graph[i][j] != candy_graph[i+1][j]:
-            candy_graph[i][j], candy_graph[i+1][j] = candy_graph[i+1][j], candy_graph[i][j]
-            ans = max(ans,max_count(candy_graph))
-            candy_graph[i][j], candy_graph[i+1][j] = candy_graph[i+1][j], candy_graph[i][j]
+        # 열 기준으로 바꾸기
+        if i+1 < n and graph[i][j] != graph[i+1][j]:
+            # 스왑해보기
+            graph[i][j], graph[i+1][j] = graph[i+1][j], graph[i][j]
+            ans = max(ans,change(graph))
+            # 다시 원상태로 복구하기
+            graph[i][j], graph[i+1][j] = graph[i+1][j], graph[i][j]
 
 
 print(ans)
