43da648b026d4cb564190152f573179f97236983
Young Hwi Kim <rladydgnj@gmail.com>
2026-03-30 17:02:32 +0900

    [Gold V] Title: 로봇 청소기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
@@ -1,76 +1,72 @@
-from collections import deque
-
 import sys
 input = sys.stdin.readline
 
 N,M = map(int,input().split())
 
-# 로봇 청소기가 있는 칸의 좌표
+# r,c,d 좌표
+# 북,동,남,서
+direction = [0,1,2,3]
 
 y,x,d = map(int,input().split())
 
 
-# 북,동,남,서 일 떄, (x,y)의 변화량
-face = [0,1,2,3]
-
 dx = [0,1,0,-1]
 dy = [-1,0,1,0]
 
-graph = []
 
-for _ in range(N):
+
+# 1. 그래프 입력 받기
+graph = [[0] * M for _ in range(N)]
+
+for i in range(N):
     line = list(map(int,input().split()))
-    graph.append(line)
+
+    for j in range(len(line)):
+        graph[i][j] = line[j]
 
 
-"""
-[[1, 1, 1], 
-[1, 0, 1],
-[1, 1, 1]]
-"""
+# 2. 시뮬레이션 돌리기
+# 0 : 청소 안됨, 1: 벽
 
-# 청소한 양
+# 청소한 횟수
 cleaned = 0
-
 while True:
-    
-    # 지금 위치한 자리에 청소해야 하는 지
+    # 현재 칸이 청소가 되지 않은 경우, 청소한다.
     if graph[y][x] == 0:
-        graph[y][x] = 2 # 청소 표시
+        graph[y][x] = 2
         cleaned +=1
 
-    # 화전해야 하는 지 여부 체크
+    # 4방향 탐색하기
+    # 90 도 회전하면서 탐색, 반시계
+
     turned = False
 
-    # 왼쪽부터 4방향 탐색
-    # 반시계 90도 회전이므로 -1씩 빠짐
     for i in range(4):
+        # ex) d = 0, next_d = 3
         d = (d - 1) % 4
-        
+
         new_x = x + dx[d]
         new_y = y + dy[d]
 
-        if graph[new_y][new_x] == 0: # 안 청소된 빈칸
-            # 앞으로 한칸 전진
-            x,y = new_x,new_y
+        if graph[new_y][new_x] == 0:
             turned = True
+            # 한 칸 앞으로 전진
+            x,y = new_x, new_y
             break
 
-    # 청소되지 않은 빈칸이 없는 경우
-    # 후진하기
+    # 한번도 회전을 안 했을 경우
     if not turned:
-        back = (d + 2) % 4
-        check_x = x + dx[back]
-        check_y = y + dy[back]
+        # 후진하기
+        back = (d+2) % 4
+        current_x = x + dx[back]
+        current_y = y + dy[back]
 
-        if graph[check_y][check_x] != 1:
-            x,y = check_x,check_y
+        if graph[current_y][current_x] == 1:
+            break
 
         else:
-            break
-
+            # 후진 한다
+            x,y = current_x,current_y
+    
 
 print(cleaned)
-
-
-
