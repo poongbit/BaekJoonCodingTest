6466efc27a0213207ec0802f3f69483717d5857e
Young Hwi Kim <rladydgnj@gmail.com>
2026-03-31 00:46:46 +0900

    [Gold IV] Title: 스도쿠, Time: 1592 ms, Memory: 122260 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.py
+++ 백준/Gold/2580. 스도쿠/스도쿠.py
@@ -17,57 +17,61 @@ for _ in range(9):
 4. 막히면 되돌아오기
 """
 
-blank = [(i,j) for i in range(9) for j in range(9) if graph[i][j]==0]
+# 1. 빈 칸(0) 목록 수집
+blank = [(i,j) for i in range(9) for j in range(9) if graph[i][j] == 0]
 
+# 첫번 째 빈 칸에 1~9 시도
 
-# 3번을 검증하기 위한 함수를 만들기
-# 같은 수가 중복으로 있으면 어차피 안됨
 def check_num(y,x,num):
 
-    # 행 검사
+    # 행에 중복 있는 지 체크
     if num in graph[y]:
         return False
     
 
-    # 열 체크
+    # 열에 중복 있는 지 체크
     for i in range(9):
         if graph[i][x] == num:
             return False
         
 
-    # 3 x 3 체크
-    box_y = (y//3) * 3
-    box_x = (x//3) * 3
+    # 3 x 3 박스에 중복 있는 지 확인
+    # 박스의 왼쪽 위가 시작점
+    box_y =  (y //3) * 3
+    box_x =  (x //3) * 3
 
-    for y in range(box_y,box_y+3):
-        for x in range(box_x,box_x+3):
+    
+    for y in range(box_y,box_y + 3):
+        for x in range(box_x, box_x +3):
             if graph[y][x] == num:
                 return False
-            
-    # 위 3가지를 다 검증했다면, true
+    
+    # 모든 걸 다 검증된 후 True 반환
     return True
 
 
 
+
 def DFS(index):
-    # 인덱스가 blank를 다 채우면
+
     if index == len(blank):
         for row in graph:
             print(*row)
 
         sys.exit()
 
-    # blank에서 비어있는 공간 꺼내기
+    # 빈칸에 채우기 위한 y,x 불러오기
+
     y,x = blank[index]
 
-    # 숫자를 하나씩 집어넣음
+    # 1~9 숫자 작성하기
     for num in range(1,10):
         if check_num(y,x,num):
             graph[y][x] = num
             DFS(index+1)
-            # 백트래킹으로 다시 복구
+            # 다시 백트래킹
             graph[y][x] = 0
 
-  
-DFS(0)
 
+
+DFS(0)
