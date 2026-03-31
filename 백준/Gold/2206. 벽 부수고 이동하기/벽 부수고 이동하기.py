6b4cbbeb3f28c895633fc2b03b3e4a5a4add047a
Young Hwi Kim <rladydgnj@gmail.com>
2026-03-31 15:38:26 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 1840 ms, Memory: 302540 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -19,58 +19,51 @@ for i in range(N):
 """
 
 def BFS():
+
+    # 무환 회귀를 막기 위한 방문 도장 생성
+    visited = [[[False] * 2 for _ in range(M)] for _ in range(N)]
+
+    # 초기 노드는 방문
+    # 방문을 부순 경우와 아닌 경우를 ([False],[False]) 로 구분
+    visited[0][0][0] = True
+
+    # 벽을 한번 부수고 갈 수 있음
+    is_broken = False
+
+    # 초기 x,y 좌표, 거리
+    q = deque([(0,0,1,is_broken)])
+
     # 상,하,좌,우
     dx = [0,0,-1,1]
     dy = [-1,1,0,0]
 
-    # 이동거리
-    
-    # 벽 한번 부섰는 지 여부
-    break_walls = False
-    q = deque([(0,0,1,break_walls)])
-
-    visited = [[[False]*2 for _ in range(M)] for _ in range(N)]
-
-    visited[0][0][0] = True
-    
-
     while q:
-        now_x,now_y,d,is_broken = q.popleft()
+        now_x,now_y,distance, is_broken = q.popleft()
 
+        # 종료 조건을 앞에 두기
         if now_x == M-1 and now_y == N-1:
-            return d
+            return distance 
 
-        
         # 4방향 탐색
 
         for i in range(4):
-            new_x = now_x + dx[i]
-            new_y = now_y + dy[i]
+            x = now_x + dx[i]
+            y = now_y + dy[i]
 
-            # map 안에 위치해 있고
-            if 0<=new_x<M and 0<=new_y<N:
-                # 만약에 도착했으면 종료
-                
-                # 0이 존재하면 갈 수 있음
-                if graph[new_y][new_x] == 0 and not visited[new_y][new_x][is_broken]:
-                    visited[new_y][new_x][is_broken] = True
-                    q.append((new_x,new_y,d+1,is_broken))
+            if 0<=x<M and 0<=y<N:
+                # 만약 0이 있다면 그곳으로 이동
+                if graph[y][x] == 0 and not visited[y][x][is_broken]:
+                    visited[y][x][is_broken] = True
+                    q.append((x,y,distance+1,is_broken))
 
-                # 벽이 있어도 한번은 부술 수 있음
+                # 1이 있지만, is_broken이 False인 경우
 
-                elif graph[new_y][new_x] == 1 and not is_broken and not visited[new_y][new_x][1]:
-                    visited[new_y][new_x][1] = True
-                    q.append((new_x,new_y,d+1,True))
+                elif graph[y][x] == 1 and not is_broken and not visited[y][x][is_broken]:
+                    visited[y][x][is_broken] = True
+                    q.append((x,y,distance+1,True))
 
     return -1
-
 
 result = BFS()
 
 print(result)
-
-
-
-            
-
-
