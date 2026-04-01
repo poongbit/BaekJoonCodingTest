d329c2cd6083eb862fc5081e3fee6520d9982a88
Young Hwi Kim <rladydgnj@gmail.com>
2026-04-01 16:09:14 +0900

    [Gold IV] Title: LCS, Time: 440 ms, Memory: 57628 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -17,15 +17,17 @@ word2 = word_list[1]
 
 dp = [[0]*(len(word2)+1) for _ in range(len(word1)+1)]
 
+# if word[i] == word[j]
 
-
-for i in range(1,len(word1)+1):
-    for j in range(1,len(word2)+1):
-        if word1[i-1] == word2[j-1]:
-            dp[i][j] = dp[i-1][j-1]  + 1
+for row in range(1,len(word1)+1):
+    for column in range(1,len(word2)+1):
+        
+        # 글자가 같으면
+        if word1[row-1] == word2[column-1]:
+            dp[row][column] = dp[row-1][column-1] + 1
 
         else:
-            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+            dp[row][column] = max(dp[row-1][column],dp[row][column-1])
 
 
 print(dp[-1][-1])
