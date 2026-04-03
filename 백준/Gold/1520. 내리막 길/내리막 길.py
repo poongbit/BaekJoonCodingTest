0f33b7cb86ec0dc4462d2ad1c92c76ad63f5304c
Young Hwi Kim <rladydgnj@gmail.com>
2026-04-03 19:29:59 +0900

    [Gold III] Title: 내리막 길, Time: 124 ms, Memory: 47192 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.py
+++ 백준/Gold/1520. 내리막 길/내리막 길.py
@@ -1,81 +1,58 @@
 import sys
 input = sys.stdin.readline
 sys.setrecursionlimit(500 * 500 + 100)
+# 경로의 개수? 이거 dp 활용?
+
 """
-한 칸 - 한 지점
-각 칸마다 그 지점의 높이, 이동 - 상 하 좌 우 
+ dp[row][column] : row와 column에 도달 할 수 있는
+ 경우의 수
 
-세준 : 제일 왼쪽 위
-
-맨 오른쪽 아래로 가는 걸 목표:
-가능한 항상 높이가 더 낮은 지점으로만 이동
-
-내리막길로만 이동하는 경로의 개수?
 
 """
 
+# 세로 : M, 가로 : N
 M,N = map(int,input().split())
 
 graph = []
 
 for _ in range(M):
-    number_list = list(map(int,input().split()))
-    graph.append(number_list)
+    line = list(map(int,input().split()))
+
+    graph.append(line)
 
 
-"""
-dp[row][column] : row,column에 위치할 때, 내리막길로 이동한 경로의 개수
 
 
-DFS 구성
-
-DFS(x,y)좌표 구성
-dp[y][x]가 방문하지 않은 곳이라면 방문 체크
-
-인덱스를 담을 리스트 
-
-4 방향 탐색:
-    현재 위치보다 작은 높이를 가진 인덱스 기록
-
-인덱스 안 리스트를 꺼내서:
-    그 리스트가 방문되지 않았으면:
-        dp[인덱스] += dp(그 이전 인덱스)
-        DFS(새로운 x,y)
+# 방문 안한 경우를 -1로 체크해서 표시
+dp = [[-1] * (N+1) for _ in range(M+1)]
 
 
-"""
-
-dp = [[-1] * N for _ in range(M)]
-
-
-def DFS(x,y):
-    # 만약, 도착지에 도착했다면, 1을 반환
-    if x == N-1 and y == M-1:
-        dp[y][x] = 1
-        return dp[y][x]
+def DFS(row,column):
+    if row == M-1 and column == N-1:
+        dp[row][column] = 1
+        return dp[row][column]
     
-    # memorization 활용
-    if dp[y][x] != -1:
-        return dp[y][x]
+    elif dp[row][column] != -1:
+        return dp[row][column]
     
-    # 방문 했으므로, -1에서 0으로 전환 
+    dp[row][column] = 0
 
-    dp[y][x] = 0
-
-    # 상,하,좌,우
     dx = [0,0,-1,1]
     dy = [-1,1,0,0]
 
     for i in range(4):
-        new_x = x + dx[i]
-        new_y = y + dy[i]
+        new_row = row + dy[i]
+        new_column = column + dx[i]
 
-        if 0<=new_x<N and 0<=new_y<M:
-            if graph[new_y][new_x] < graph[y][x]:
-                dp[y][x] += DFS(new_x,new_y)
+        if 0<=new_row<M and 0<=new_column<N:
 
-    return dp[y][x]
+            if graph[new_row][new_column] < graph[row][column]:
+                 dp[row][column] += DFS(new_row,new_column)
 
-DFS(0,0)
+
+    return dp[row][column]
+
 
 print(DFS(0,0))
+
+
