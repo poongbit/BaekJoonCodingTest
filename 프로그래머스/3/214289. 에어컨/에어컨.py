75e97aa754ba86e87d9baebc5ea22e601c15ec49
Young Hwi Kim <rladydgnj@gmail.com>
2026-04-03 23:50:08 +0900

    [level 3] Title: 에어컨, Time: 148.57 ms, Memory: 9.84 MB -BaekjoonHub

--- 프로그래머스/3/214289. 에어컨/에어컨.py
+++ 프로그래머스/3/214289. 에어컨/에어컨.py
@@ -1,92 +1,104 @@
 def solution(temperature, t1, t2, a, b, onboard):
     
     """
-    쾌적 온도 t1~t2 유지 (승객 탑승중일 때)    
-
-    에어컨 on - 희망온도와 같아지는 방향 +-1, 전력 a 소모
-            실내오도가 같다면 켜져 있는 동안 온도 안바뀜 - 전력 b 소모
+    실내온도 t1,t2 - 승객 탑승 중일 때
     
-    에어컨 off - 실외온도와 같아지는 방향으로 +-1
-            실내 온도와  같다면 안 변함 전력 0 소모
-            
+    희망온도 - 전원 켜져있는 동안 원하는 값으로 변경 가능
     
-    dp[t][temp] : t분에서 temp 온도 일 때 소모되는 최소 전기량        
+    에어컨 on -> 실내온도 != 희망온도 -> 희망온도로 +-1 / 전력 a 소비
+                같으면 변화x / 전력 소모 b
+                
+    에어컨 off -> 실내온도 -> 실외온도와 같아지는 방향 +-1 / 전력 0 소모
+                같으면 변화 x
+    
+    켜고 끄는데 전력 소모 0 가정
+    
+    목표 : 승객 탑승 중일 때 t1~t2 유지 + 에어컨 소비전력 최소화
+                
+    
+    실외온도 = temperature
+    실내 온도 범위 t1,t2
+    에어컨 소비 전력 a,b
+    승객이 탑승중인 시간 1차원 정수 배열 onboard
+    
+    탑승중인 시간에 쾌적한 실내온도 유지를 위한 최소 소비 전력?
     
     """
     
+    # DFS BFS 하기에는 조건에 따라서 전력 소모가 왔다 갔다 함
+    # 아예 값 자체를 저장 다 해버리는 걸로 해볼까? DP로
+    
+    # dp[i][temp] = i분 때에 temp 온도일 떄 소모되는 최소 전력
+    
+    """
+    1. dp 배열 선언
+    2. 전력이 소모되는 케이스를 저장해서 각각 꺼낸다.
+    3. 조건이 맞는 선에서 전력이 가장 적게 드는 쪽으로 고른다.
+    
+    """
+    
+    T = len(onboard) # 시간
+    # 최소를 구하는 거니 INF를 설정
     INF = float('inf')
-    T = len(onboard)
-    # 인덱스에 영하 10도를 표현할 수 없으므로, offset 착용
-    offset = 10
-    diverse = 52
     
-    # dp 배열 선언
-    dp = [[INF] * diverse  for _ in range(T)]
+    dp = [[INF]*52 for _ in range(T+1)]
+    offset = 10 # temp에 들어갈 인덱스가 음수이면 입력이 안됨
     
-    # 0분,기온일 때 전력 소모량
-    dp[0][temperature+offset] = 0
+    # 초기값 설정
+    # 현재 0분은 실내온도와 실외온도가 같음, 전력소모 0
+    dp[0][offset+temperature] = 0
     
-    # 시간마다, 기온 체크
+    # 2. 전력이 소모되는 케이스
     
-    for time in range(T-1):
-        for temp in range(-10,41):
-            # 방문하지 않은 시간당 temp는 체크하지 않음
-            if dp[time][offset+ temp] == INF:
+    # t분 동안, 온도의 변화량
+    for t in range(T-1):
+        for temp in range(-10,41): # 희망 온도 설정중
+            
+            # 도달하지 못한 곳은 그냥 넘김
+            if dp[t][offset+temp] == INF:
                 continue
             
-            # 이 시간 당시의 온도에 따른 전력 소모량 체크
-            cost = dp[time][offset + temp]
+            # 도달한 곳에서 (t분일 때 온도 temp)
+            cost = dp[t][offset+temp]
             
-            # 전이 할 수 있는 것들 체크
-            check = []
+            candidates = [] # 전이 후보 설정
             
-            # 1. 에어컨이 꺼저 있을 때
+            # 1. 에어컨이 켜져 있을 때
+            # 온도를 미리 낮추거나 높힘으로써 조절할 수 있으므로 경우 셋 다 포함
+            candidates.append((temp+1,a))
+            candidates.append((temp-1,a))
+            candidates.append((temp,b))
             
-            if temp < temperature: # 희망 온도가 외부 온도보다 낮다면:
-                check.append((temp + 1,0))
+            # 2. 에어컨이 꺼져있을 떄
+            if temp > temperature:
+                candidates.append((temp-1,0))
                 
-            elif temp > temperature: # 희망 온도가 외부 온도보다 높다면:
-                check.append((temp-1,0))
+            elif temp < temperature:
+                candidates.append((temp+1,0))
                 
             else:
-                check.append((temp,0))
-                
-                
-            # 2. 에어컨이 켜져 있을 때
-            # 에어컨이 켜져 있을 때는 자유롭게 켜고 끌 수 있음
+                candidates.append((temp,0))
             
-            check.append((temp+1,a))
-            check.append((temp-1,a))
-            check.append((temp,b))
             
-            # 각각 꺼내서, 필요한 상황인지 체크
-            for next_temp , add_cost in check:
+            for next_temp, add_cost in candidates:
                 
-                # next_temp가 -10에서 40 사이에 있지 못할 경우
-                if not (-10<=next_temp<=40):
+                # 사람이 안 타고 있을 때는 t1,t2 굴레는 없지만, 그래도 최소
+                # 온도는 지켜야 함
+                if not(-10<=next_temp<=40):
                     continue
                 
-                
-                # 다음 승객이 타 있는데 범위 안에 못 들어가면 넘김
-                if onboard[time+1] == 1 and not (t1<=next_temp<=t2):
+                # 사람이 탔는데, t1,t2 밖에 있다면 안됨
+                if onboard[t+1] == 1 and not(t1<= next_temp <= t2):
                     continue
                     
-                # 다음 온도를 표기하기 위한 인덱스 선언
                 idx = next_temp + offset
-                
-                # 기존에 적힌 비용과 새롭게 계산된 비용과 비교해서 작은 것
-                dp[time+1][idx] = min(dp[time+1][idx], cost + add_cost)
-                
-                
+                    
+                dp[t+1][idx] = min(dp[t+1][idx], cost+add_cost) 
+                    
+    
+    
     return min(dp[T-1])
-    
-    
-            
-            
-            
-            
-    
-    
+        
     
     
     
