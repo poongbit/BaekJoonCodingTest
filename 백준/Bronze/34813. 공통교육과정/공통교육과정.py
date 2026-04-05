cc577ed1083e1d95b15ac4a84205043cda41a8ff
Young Hwi Kim <rladydgnj@gmail.com>
2026-04-06 04:16:21 +0900

    [Bronze V] Title: 공통교육과정, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/34813. 공통교육과정/공통교육과정.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+word = str(input().strip())
+
+letter = word[0]
+
+if letter == 'F':
+    print('Foundation')
+
+elif letter == 'C':
+    print('Claves')
+
+elif letter =='V':
+    print('Veritas')
+
+elif letter =='E':
+    print('Exploration')
